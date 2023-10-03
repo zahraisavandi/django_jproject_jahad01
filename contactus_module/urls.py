b51ae85d3fce47b16bd9383e0c6128502e77a1ac
zahraisavandi <146559068+zahraisavandi@users.noreply.github.com>
2023-10-03 15:27:55 +0100

    adding first commit

--- contactus_module/urls.py
+++ contactus_module/urls.py
@@ -3,7 +3,7 @@ from . import views
 app_name='contactus_module'
 urlpatterns=[
     path('',views.contactus,name='contact-us'),
-    path('register/',views.user_register,name='user-register'),
+    path('register/',views.user_register,name='user-register')
     # path('login/',views.user_login,name='user-login'),
     # path('logout/',views.user_logout,name='user-logout'),
 ]
--- contactus_module/views.py
+++ contactus_module/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from . forms import UserRegisterForm,UserLoginForm
+from . forms import UserRegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 # Create your views here.
